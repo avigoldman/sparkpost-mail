99e1bf63b3aa5699760938e2c09b893ae7962317
Avi Goldman <avrahamymgoldman@gmail.com>
2016-05-27 09:36:05 -0400

    setup functions

--- sparkpost-mail.js
+++ sparkpost-mail.js
@@ -2,4 +2,12 @@
 
 // Variables exported by this module can be imported by other packages and
 // applications. See sparkpost-mail-tests.js for an example of importing.
-export const name = 'sparkpost-mail';
+
+export var Sparkpost = {
+	config: function(options) {
+		
+	},
+	send: function(options) {
+		Email.send(options);
+	}
+};
